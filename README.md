0e53b0095cf9c81e99581c666b2776d0b818a3d8
ogbeniore <oreeboy@gmail.com>
2019-02-22 12:42:23 +0100

    added setup for build

---
+++ README.md
@@ -0,0 +1,5 @@
+# Atlas SMS Python 
+
+This is a simple python wrapper for the Atlas sms API. You can use the 
+[Documentation](https://sms.atlas.money/docs)
+on how to use the API.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atlas-sms-python",
+    version="0.0.1",
+    author="Oreoluwa Ojo ",
+    author_email="oreeboy@gmail.com",
+    description="A simple wrapper for the Atlas SMS Gateway",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ogbeniore/atlas-sms-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
