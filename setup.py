5c407f9adda90d04ee8f0a5af32882196e3aac79
ogbeniore <oreeboy@gmail.com>
2019-02-22 17:48:21 +0100

    changed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="atlas-sms-python",
+    name="atlas-sms-test",
     version="0.0.1",
     author="Oreoluwa Ojo ",
     author_email="oreeboy@gmail.com",
